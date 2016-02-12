7121ccf4d1e7bbbd8efb66a5f558c5473db47dec
Daniel Franco <danielsbfranco@gmail.com>
2016-02-12 17:27:58 -0200

    added fields to CriadoEvent

--- generators/app/templates/spm-backend/src/main/java/com/thunderbees/spm/_CriadoEvent.java
+++ generators/app/templates/spm-backend/src/main/java/com/thunderbees/spm/_CriadoEvent.java
@@ -1,28 +1,35 @@
 package com.thunderbees.spm.<%=pacote%>;
 
-  import arquitetura.geral.empresa.EmpresaId;
-  import arquitetura.impl.AbstractEventGeneric;
+import arquitetura.geral.empresa.EmpresaId;
+import arquitetura.impl.AbstractEventGeneric;
 
-public class<%=classe%>CriadoEvent extends AbstractEventGeneric<Criar<%=classe%>Command>{
+public class <%=classe%>CriadoEvent extends AbstractEventGeneric<Criar<%=classe%>Command> {
 
-private static final long serialVersionUID=1L;
+    private static final long serialVersionUID = 1L;
 
-private<%=classe%>Id entityId;
+    private <%=classe%>Id entityId;
+<%for(fieldId in fields){%>
+    private <%=fields[fieldId].fieldType%> <%=fields[fieldId].fieldName%>;
+<%}%>
+    private EmpresaId contextEmpresaId;
 
-private EmpresaId contextEmpresaId;
+    public<%=classe%>CriadoEvent(Criar<%=classe%>Command sourceMessage) {
+        super(sourceMessage);
+        entityId = sourceMessage.getEntityId();<%for(fieldId in fields) {%>
+        this.<%=fields[fieldId].fieldName%> = <%=fields[fieldId].fieldName%>;<%}%>
+        contextEmpresaId = sourceMessage.getContextEmpresaId();
+    }
 
-public<%=classe%>CriadoEvent(Criar<%=classe%>Command sourceMessage){
-  super(sourceMessage);
-  entityId=sourceMessage.getEntityId();
-  contextEmpresaId=sourceMessage.getContextEmpresaId();
-  }
+    public<%=classe%>Id getEntityId() {
+        return entityId;
+    }
+<%for(fieldId in fields){%>
+    public <%=fields[fieldId].fieldType%> get<%=fields[fieldId].capitalizedFieldName%>() {
+        return this.<%=fields[fieldId].fieldName%>;
+    }
+<%}%>
+    public EmpresaId getContextEmpresaId() {
+        return contextEmpresaId;
+    }
 
-public<%=classe%>Id getEntityId(){
-  return entityId;
-  }
-
-public EmpresaId getContextEmpresaId(){
-  return contextEmpresaId;
-  }
-
-  }
+}
