f05a83018a868de2d53ad3d51404979aacd56454
Daniel Franco <danielsbfranco@gmail.com>
2016-02-12 17:06:30 -0200

    Adding fields to CriarCommand

--- generators/app/templates/spm-backend/src/main/java/com/thunderbees/spm/_CriarCommand.java
+++ generators/app/templates/spm-backend/src/main/java/com/thunderbees/spm/_CriarCommand.java
@@ -3,25 +3,31 @@ package com.thunderbees.spm.<%=pacote%>;
   import com.fasterxml.jackson.annotation.JsonCreator;
   import com.fasterxml.jackson.annotation.JsonProperty;
   import com.thunderbees.spm.BasicAbstractCommand;
-
   import arquitetura.geral.empresa.EmpresaId;
 
-public class Criar<%=classe%>Command extends BasicAbstractCommand<<%=classe%>Id>{
+public class Criar<%=classe%>Command extends BasicAbstractCommand<<%=classe%>Id> {
 
 private static final long serialVersionUID=1L;
+<%for(fieldId in fields){%>
+    private <%=fields[fieldId].fieldType%> <%=fields[fieldId].fieldName%>;
+<%}%>
+    public Criar<%=classe%>Command() {
+        entityId = new <%=classe%>Id();
+    }
 
-public Criar<%=classe%>Command(){
-  entityId=new<%=classe%>Id();
-  }
+    @JsonCreator
+    public Criar<%=classe%>Command(<%for(fieldId in fields) {%>@JsonProperty("<%=fields[fieldId].fieldName%>") <%=fields[fieldId].fieldType%> <%=fields[fieldId].fieldName%>, <%}%>@JsonProperty("contextEmpresaId") EmpresaId contextEmpresaId) {
+        this(new <%=classe%>Id(), contextEmpresaId<%for(fieldId in fields) {%>, <%=fields[fieldId].fieldName%><%}%>);
+    }
 
-@JsonCreator
-public Criar<%=classe%>Command(@JsonProperty("contextEmpresaId")EmpresaId contextEmpresaId){
-  this(new<%=classe%>Id(),contextEmpresaId);
-  }
-
-public Criar<%=classe%>Command(<%=classe%>Id entityId,EmpresaId contextEmpresaId){
-  this.entityId=entityId;
-  this.contextEmpresaId=contextEmpresaId;
-  }
-
-  }
+    public Criar<%=classe%>Command(<%=classe%>Id entityId, <%for(fieldId in fields) {%><%=fields[fieldId].fieldType%> <%=fields[fieldId].fieldName%>, <%}%>EmpresaId contextEmpresaId) {
+        this.entityId = entityId;
+        this.contextEmpresaId = contextEmpresaId;<%for(fieldId in fields) {%>
+        this.<%=fields[fieldId].fieldName%> = <%=fields[fieldId].fieldName%>;<%}%>
+    }
+<%for(fieldId in fields){%>
+    public <%=fields[fieldId].fieldType%> get<%=fields[fieldId].capitalizedFieldName%>() {
+        return this.<%=fields[fieldId].fieldName%>;
+    }
+<%}%>
+}
