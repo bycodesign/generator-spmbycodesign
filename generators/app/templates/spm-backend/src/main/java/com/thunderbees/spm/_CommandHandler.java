9d6f004846484e10294ab4dc41a960d8cfd8957e
Daniel Franco <danielsbfranco@gmail.com>
2016-02-12 17:18:46 -0200

    Formatting corrections on Command  Handler

--- generators/app/templates/spm-backend/src/main/java/com/thunderbees/spm/_CommandHandler.java
+++ generators/app/templates/spm-backend/src/main/java/com/thunderbees/spm/_CommandHandler.java
@@ -1,57 +1,57 @@
 package com.thunderbees.spm.<%=pacote%>;
 
-  import javax.transaction.Transactional;
+import javax.transaction.Transactional;
 
-  import org.slf4j.Logger;
-  import org.slf4j.LoggerFactory;
-  import org.springframework.beans.factory.annotation.Autowired;
-  import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-  import arquitetura.api.AbstractCommandHandler;
-  import arquitetura.api.annotations.CommandHandler;
-  import arquitetura.api.annotations.MessageHandler;
-  import arquitetura.impl.AbstractCommand;
+import arquitetura.api.AbstractCommandHandler;
+import arquitetura.api.annotations.CommandHandler;
+import arquitetura.api.annotations.MessageHandler;
+import arquitetura.impl.AbstractCommand;
 
 @Component
 @MessageHandler
 @Transactional
-public class<%=classe%>CommandHandler extends AbstractCommandHandler{
+public class<%=classe%>CommandHandler extends AbstractCommandHandler {
 
-private static final Logger logger=LoggerFactory.getLogger(<%=classe%>CommandHandler.class);
+    private static final Logger logger = LoggerFactory.getLogger(<%=classe%>CommandHandler.class);
 
-private<%=classe%>Repository repository;
+    private <%=classe%>Repository repository;
 
-@Autowired
-public void setRepository(<%=classe%>Repository repository){
-  this.repository=repository;
-  }
+    @Autowired
+    public void setRepository(<%=classe%>Repository repository) {
+        this.repository=repository;
+    }
 
-public<%=classe%>Repository getRepository(){
-  return repository;
-  }
+    public <%=classe%>Repository getRepository() {
+        return repository;
+    }
 
-@CommandHandler
-public void handle(Criar<%=classe%>Command command){
-  log(command);
+    @CommandHandler
+    public void handle(Criar<%=classe%>Command command) {
+        log(command);
 
-<%=classe%><%=variavel%>=new<%=classe%>(command);
-  getRepository().save(<%=variavel%>);
+        <%=classe%> <%=variavel%> = new <%=classe%>(command);
+        getRepository().save(<%=variavel%>);
 
-  addEventsToDispatchFromEntity(<%=variavel%>);
-  }
+        addEventsToDispatchFromEntity(<%=variavel%>);
+    }
 
-@CommandHandler
-public void handle(Editar<%=classe%>Command command){
-  log(command);
+    @CommandHandler
+    public void handle(Editar<%=classe%>Command command) {
+        log(command);
 
-<%=classe%><%=variavel%>=getRepository().findOne(command.getEntityId());
-<%=variavel%>.handle(command);
-  getRepository().save(<%=variavel%>);
+        <%=classe%> <%=variavel%> = getRepository().findOne(command.getEntityId());
+        <%=variavel%>.handle(command);
+        getRepository().save(<%=variavel%>);
 
-  addEventsToDispatchFromEntity(<%=variavel%>);
-  }
+        addEventsToDispatchFromEntity(<%=variavel%>);
+    }
 
-private void log(AbstractCommand command){
-  logger.debug("Processing {} in {}.",command,this);
-  }
-  }
+    private void log(AbstractCommand command){
+        logger.debug("Processing {} in {}.", command, this);
+    }
+}
